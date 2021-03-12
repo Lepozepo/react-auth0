a8981b358b30ed9a5a16830752aa2b3c486e8d19
Marcelo Reyna <Lepozepo@gmail.com>
2021-03-12 00:02:27 -0800

    Patch linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,8 +7,10 @@ module.exports = {
   //   'jest',
   // ],
   rules: {
+    'react/jsx-props-no-spreading': 0,
     'no-underscore-dangle': 0,
-    'react/jsx-filename-extension': [1, { 'extensions': ['.js', '.jsx'] }],
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'import/no-unresolved': ['error', { commonjs: true, caseSensitive: true, ignore: ['react', 'react-dom'] }],
   },
   parser: 'babel-eslint',
   env: {
